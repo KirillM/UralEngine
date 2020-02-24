04ef5c77eae3079ee6513b2daa10e5ab4bdfaec0
Кирилл Мезрин <mezrink@gmail.com>
2020-02-25 04:30:57 +0500

    Refactoring: links to externals added

---
+++ UralEngine/vendor/stb_image/stb_image.cpp
@@ -0,0 +1,10 @@
+//
+//  stb_image.cpp
+//  UralEngine
+//
+//  Created by Кирилл Мезрин on 25.02.2020.
+//  Copyright © 2020 Кирилл Мезрин. All rights reserved.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
