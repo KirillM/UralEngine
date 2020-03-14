f6cb3a90bd75724cbbf718bd1a2686f96b310d03
Кирилл Мезрин <mezrink@gmail.com>
2020-03-14 23:50:01 +0500

    Shader

---
+++ UralEngine/working/assets/shaders/Identity.glsl
@@ -0,0 +1,29 @@
+
+#type vertex
+
+#version 330
+
+// внутри себя шейдеры работают с vec4
+
+in vec4 a_Postion; // в каждом шейдере можно иметь максимум 16 аттрибутов
+in vec4 a_Color;
+
+out vec4 v_Color;
+
+void main(void) {
+    v_Color = a_Color
+    gl_Position = a_Position
+}
+
+
+#type fragment
+
+#version 330
+
+in vec4 a_Color // в каждом шейдере можно иметь максимум 16 аттрибутов
+
+out vec4 v_FragColor // любой out идет в цвет fragment
+
+void main(void) {
+    v_FragColor = a_Color
+}
